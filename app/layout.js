c65487a882b655e38f7901c7329dd9da88c2603b
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2026-04-11 12:29:51 +0530

    Added Analtycis

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
+import Script from "next/script";
 
 export const metadata = {
   title: "ThitaInfo Games - Play & Compete",
@@ -10,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          src="http://72.60.223.198:32768/script.js"
+          data-website-id="770d3ab4-70ac-4c4f-83cc-fa5367c1b422"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="font-poppins antialiased">
         <ErrorBoundary>{children}</ErrorBoundary>
       </body>
